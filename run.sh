7aa0f4a27572a0b9bf100bb442037c2e3a46baec
juergens <juergens>
2018-08-10 11:10:34 +0200

    bugfix: os.symlink doesn't work for some reason (replaced it with ln -s), also source gets deleted (instead of moved) if it already exists in the archive

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-spark-submit --master yarn --deploy-mode client --jars `pwd`/target/sparkhacks-0.0.1-SNAPSHOT.jar dirhash.py "$@"
+spark-submit --master yarn --deploy-mode client --jars `pwd`/target/sparkhacks-0.0.1-SNAPSHOT.jar dirhash.py "$@" 2>/dev/null | sed '/^spark/d'
+
+# Note: sed is used to filter a single warning, that spark prints to stdout instead stderr. I think this is a bug in spark1.4.1, and thus the sed-part can be removed once SDIL moves to a newer version of spark
+# see: https://github.com/SmartDataInnovationLab/dirhash/issues/13
